da8a60f2bc64b5338903fdc35eb523450ca66e54
Ferydun Amati <office@amati.solutions>
2022-11-03 14:52:56 +0100

    make wave flexible

--- components/LimvilWave.vue
+++ components/LimvilWave.vue
@@ -1,6 +1,19 @@
 <template>
   <svg version="1.1" viewBox="0 0 1440 100" xmlns="http://www.w3.org/2000/svg">
-    <path d="m0 100v-99c195-1.6 390-3.2 550 2.8 160 5.9 286 19 429 20 143 0.79 302-11 461-23v99z"
-      fill="rgba(255, 255, 255, var(--trans))" stroke-width="0" />
+    <path :d="path" fill="rgba(255, 255, 255, var(--trans))" stroke-width="0" />
   </svg>
 </template>
+
+<script setup lang="ts">
+import { rand } from '@vueuse/core';
+
+const pos1 = ref<number>(0)
+const pos2 = ref<number>(0)
+const path = ref<string>('')
+
+onMounted(() => {
+  pos1.value = rand(1, 85)
+  pos2.value = rand(-4, 80)
+  path.value = `M 0,100 V 1 c 195,-1.6 390,${pos1.value} 550,${pos1.value + 6} 160,5.9 286,${pos2.value - pos1.value + 4} 429, ${pos2.value - pos1.value + 5} 143, 0.79 302, 9.1 461, -2.9 v 99 z`
+})
+</script>
