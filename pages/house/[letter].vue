cfd9203541744df528bdce9fdfd3c9360bcc2bac
Ferydun Amati <office@amati.solutions>
2022-11-03 15:24:29 +0100

    small house changes

--- pages/house/[letter].vue
+++ pages/house/[letter].vue
@@ -59,24 +59,26 @@ onMounted(() => {
         <img lazy v-if="house.logo" :src="`${config.public.directusBase}/assets/${house.logo}`">
       </p>
       <h2>{{ localeHouse.name }}</h2>
-      <div v-html="localeHouse.description" />
-      <div class="house-furnishing-and-prices">
-        <div>
-          <h3>{{ $t('furnishing') }}</h3>
-          <p style="white-space: pre;">{{ localeHouse.furnishing }}</p>
-        </div>
-        <div>
-          <h3>{{ $t('prices_per_night') }}</h3>
-          <table class="prices-table">
-            <tr v-for="(s, idx) in prices.seasons" :key="s.name" :class="s === activeSeason ? 'active-season' : ''">
-              <td>{{ $d(Date.parse(s.start), 'text_short_no_month') }}</td>
-              <td>{{ $t('to') }}</td>
-              <td>{{ $d(Date.parse(s.end), 'text_short_no_month') }}</td>
-              <td v-if="idx === 0 || idx === 4">{{ $n(house.price_off_season, 'currency') }}</td>
-              <td v-else-if="idx === 1 || idx === 3">{{ $n(house.price_pre_season, 'currency') }}</td>
-              <td v-else-if="idx === 2">{{ $n(house.price_main_season, 'currency') }}</td>
-            </tr>
-          </table>
+      <div class="house-infos">
+        <div v-html="localeHouse.description" />
+        <div class="house-furnishing-and-prices">
+          <div>
+            <h3>{{ $t('furnishing') }}</h3>
+            <p style="white-space: pre;">{{ localeHouse.furnishing }}</p>
+          </div>
+          <div>
+            <h3>{{ $t('prices_per_night') }}</h3>
+            <table class="prices-table">
+              <tr v-for="(s, idx) in prices.seasons" :key="s.name" :class="s === activeSeason ? 'active-season' : ''">
+                <td>{{ $d(Date.parse(s.start), 'text_short_no_month') }}</td>
+                <td>{{ $t('to') }}</td>
+                <td>{{ $d(Date.parse(s.end), 'text_short_no_month') }}</td>
+                <td v-if="idx === 0 || idx === 4">{{ $n(house.price_off_season, 'currency') }}</td>
+                <td v-else-if="idx === 1 || idx === 3">{{ $n(house.price_pre_season, 'currency') }}</td>
+                <td v-else-if="idx === 2">{{ $n(house.price_main_season, 'currency') }}</td>
+              </tr>
+            </table>
+          </div>
         </div>
       </div>
     </div>
@@ -90,18 +92,23 @@ onMounted(() => {
   min-height: 100vh;
   min-height: 100dvh;
   display: grid;
-  justify-content: center;
+  justify-content: stretch;
   align-content: start;
 }
 
 .house-wave {
-  margin-top: calc(100vh - 240px);
+  margin-top: calc(100vh - 280px);
 }
 
 .house-content {
   padding: 32px;
-  background-color: rgba(255, 255, 255, var(--trans));
-  border-radius: 32px;
+  background: linear-gradient(rgba(255, 255, 255, var(--trans)), white 200px);
+  border-radius: 0;
+  display: grid;
+  justify-content: center;
+}
+
+.house-infos {
   max-width: 600px;
 }
 
@@ -109,9 +116,6 @@ onMounted(() => {
   .house-content {
     margin: 0;
     padding: 16px;
-    background-color: rgba(255, 255, 255, var(--trans));
-    border-radius: 0;
-    max-width: 600px;
   }
 }
 
