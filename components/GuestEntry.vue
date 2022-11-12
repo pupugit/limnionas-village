d02e125418a7a78890567df8b709484df488cea8
Ferydun Amati <office@amati.solutions>
2022-11-12 02:13:49 +0100

    add GuestEntry component

---
+++ components/GuestEntry.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import type { GuestbookEntry } from '~~/types/guestbookEntry'
+const props = defineProps<{
+  entry: GuestbookEntry,
+}>()
+</script>
+
+<template>
+  <div>{{ entry.person }} um {{ entry.date_created }}</div>
+</template>
--- pages/guestbook.vue
+++ pages/guestbook.vue
@@ -1,13 +1,13 @@
 <template>
-  <div>gb
-    <div v-for="entry in guestbook">
-      <div>{{ entry.person }} um {{ entry.date_created }}</div>
-    </div>
+  <div>
+    <div>gb</div>
+    <GuestEntry v-for="e in guestbook" :entry="e" :key="e.id" />
   </div>
 </template>
 
 <script lang="ts" setup>
 import { useI18n } from 'vue-i18n'
+import GuestEntry from '~~/components/GuestEntry.vue';
 const i18n = useI18n()
 useHead({ title: i18n.t('guestbook') })
 await initGuestbook()
