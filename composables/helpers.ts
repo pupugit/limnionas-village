43d3cbeff1421f1c3d6603ee960444f5922c0ba4
Ferydun Amati <office@amati.solutions>
2022-12-19 02:05:44 +0100

    fix randInt

--- composables/helpers.ts
+++ composables/helpers.ts
@@ -1,4 +1,4 @@
-function randInt(min: number, max: number) {
+export const randInt = (min: number, max: number) => {
   min = Math.ceil(min)
   max = Math.floor(max)
   return Math.floor(Math.random() * (max - min + 1) + min)
