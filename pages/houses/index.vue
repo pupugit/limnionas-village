21767f65fd01499c42ab1ab9f34ce63bcb46ba8f
Ferydun Amati <office@amati.solutions>
2022-12-26 12:04:13 +0100

    basic implementation of the houses page

--- pages/houses/index.vue
+++ pages/houses/index.vue
@@ -1,3 +1,29 @@
 <template>
-  <div>houses</div>
+  <div>
+    <h1>{{ $t('houses') }}</h1>
+    <div v-html="localPricesText"></div>
+    <HousesTable />
+  </div>
 </template>
+
+<script setup lang="ts">
+import { useI18n } from 'vue-i18n'
+const texts = useTexts()
+const i18n = useI18n()
+
+const localPricesText = computed(() => {
+  if (i18n.locale.value === 'de' || i18n.locale.value === 'fr' || i18n.locale.value === 'en') {
+    const found = texts.value.find(t => t.id === 'prices_text')
+    if (!found) return ''
+    let ret = found.content
+    if (i18n.locale.value !== 'en') {
+      const t = found.translations.find(trans => trans.languages_code === i18n.locale.value)
+      if (t) {
+        ret = t.content
+      }
+    }
+    return ret
+  }
+  return ''
+})
+</script>
