2636f95025b62fdf5dfb4213edaaa96915108bb5
Ferydun Amati <office@amati.solutions>
2023-03-10 10:25:18 +0100

    first try

--- pages/gallery.vue
+++ pages/gallery.vue
@@ -1,8 +1,19 @@
 <template>
-  <div class="gallery-page"></div>
+  <div class="gallery-page">
+    <Splide :options="{ rewind: true }" :aria-label="gallery.name" v-if="gallery.id">
+      <SplideSlide v-for="p in gallery.fotos" :key="p.directus_files_id">
+        <img
+          :src="`${config.public.directusBase}/assets/${p.directus_files_id}?fit=inside&width=1024&height=1024&format=${config.public.imageFormat}`"
+          alt="Picture">
+      </SplideSlide>
+    </Splide>
+  </div>
 </template>
 
 <script lang="ts" setup>
+import '@splidejs/vue-splide/css'
+import '@splidejs/vue-splide/css/skyblue'
+
 import { useI18n } from 'vue-i18n'
 
 const i18n = useI18n()
