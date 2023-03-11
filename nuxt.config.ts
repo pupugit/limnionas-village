5865e926e48a7c019e9054b9589c592c3feaa0eb
Ferydun Amati <office@amati.solutions>
2023-03-11 10:18:57 +0100

    fix it

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -26,14 +26,14 @@ export default defineNuxtConfig({
       name: 'page', mode: 'out-in'
     },
   },
-  hooks: {
-    'vite:extendConfig'(config, { isServer }) {
-      if (isServer) {
-        // Workaround for netlify issue
-        // https://github.com/nuxt/framework/issues/6204
-        // @ts-ignore
-        config.build.rollupOptions.output.inlineDynamicImports = true
-      }
-    }
-  },
+  // hooks: {
+  //   'vite:extendConfig'(config, { isServer }) {
+  //     if (isServer) {
+  //       // Workaround for netlify issue
+  //       // https://github.com/nuxt/framework/issues/6204
+  //       // @ts-ignore
+  //       config.build.rollupOptions.output.inlineDynamicImports = true
+  //     }
+  //   }
+  // },
 })
