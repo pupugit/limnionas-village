39bd449a6b02c15041e6fbe0ae7c042b67c70cd2
Ferydun Amati <office@amati.solutions>
2023-03-28 23:38:48 +0200

    add hikes

--- pages/hiking.vue
+++ pages/hiking.vue
@@ -1,7 +1,8 @@
 <template>
-  <div class="arrival-page">
-    <div class="arrival-content">
-      <div v-for="h in hikes" :key="h.id">
+  <div class="hikes-page" v-if="width !== Infinity">
+    <div v-for="(h, idx) in hikes" :key="h.id" class="hikes-section"
+      :style="h.fotos.length ? `background-image: url(${config.public.directusBase}/assets/${h.fotos[0].directus_files_id}?fit=cover&width=${width}&height=${height}&withoutEnlargement&format=${config.public.imageFormat});` : ''">
+      <div class="hikes-content" :style="idx === 0 ? 'margin-top:128px;' : ''">
         <h1>{{ h.title }}</h1>
         <div v-html="h.content"></div>
       </div>
@@ -16,6 +17,7 @@ useHead({ title: i18n.t('hiking') })
 const config = useRuntimeConfig()
 await initHikes()
 const hikes = useHikes()
+const { width, height } = useWindowSize()
 </script>
 
 <style>
@@ -24,11 +26,21 @@ const hikes = useHikes()
   box-sizing: border-box;
   background-attachment: fixed;
   display: grid;
+  /* gap: 16px; */
+}
+
+.hikes-section {
+  display: grid;
+  place-content: center;
+  width: 100vw;
+  min-height: 100vh;
+  background-attachment: fixed;
+  background-color: green;
 }
 
 .hikes-content {
   background: rgba(255, 255, 255, 80%);
-  margin: 128px 10vw 32px 10vw;
+  margin: 10vw;
   padding: 2rem;
   max-width: 1000px;
   align-self: start;
