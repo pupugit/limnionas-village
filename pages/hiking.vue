b61f326a09bbd062eeefb0eceaba2215b3e7bf36
Ferydun Amati <office@amati.solutions>
2023-03-28 22:20:37 +0200

    add some basic content to hiking page

--- pages/hiking.vue
+++ pages/hiking.vue
@@ -1,6 +1,11 @@
 <template>
-  <div>
-
+  <div class="arrival-page">
+    <div class="arrival-content">
+      <div v-for="h in hikes" :key="h.id">
+        <h1>{{ h.title }}</h1>
+        <div v-html="h.content"></div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -12,3 +17,21 @@ const config = useRuntimeConfig()
 await initHikes()
 const hikes = useHikes()
 </script>
+
+<style>
+.hikes-page {
+  min-height: 100vh;
+  box-sizing: border-box;
+  background-attachment: fixed;
+  display: grid;
+}
+
+.hikes-content {
+  background: rgba(255, 255, 255, 80%);
+  margin: 128px 10vw 32px 10vw;
+  padding: 2rem;
+  max-width: 1000px;
+  align-self: start;
+  justify-self: center;
+}
+</style>
