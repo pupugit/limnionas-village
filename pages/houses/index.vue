34167608442141f202aab23cedcc01947e1548aa
Ferydun Amati <office@amati.solutions>
2023-03-30 18:55:58 +0200

    add pricestext2

--- pages/houses/index.vue
+++ pages/houses/index.vue
@@ -2,6 +2,8 @@
   <div class="default-page">
     <h1>{{ $t('houses') }}</h1>
     <div v-html="localPricesText"></div>
+    <h1>{{ $t('prices') }}</h1>
+    <div v-html="localPricesText2"></div>
     <HousesTable style="margin-top:4em;" />
   </div>
 </template>
@@ -17,6 +19,21 @@ const texts = useTexts()
 const localPricesText = computed(() => {
   if (i18n.locale.value === 'de' || i18n.locale.value === 'fr' || i18n.locale.value === 'en') {
     const found = texts.value.find(t => t.id === 'prices_text')
+    if (!found) return ''
+    let ret = found.content
+    if (i18n.locale.value !== 'en') {
+      const t = found.translations.find(trans => trans.languages_code === i18n.locale.value)
+      if (t) {
+        ret = t.content
+      }
+    }
+    return ret
+  }
+  return ''
+})
+const localPricesText2 = computed(() => {
+  if (i18n.locale.value === 'de' || i18n.locale.value === 'fr' || i18n.locale.value === 'en') {
+    const found = texts.value.find(t => t.id === 'prices_text2')
     if (!found) return ''
     let ret = found.content
     if (i18n.locale.value !== 'en') {
