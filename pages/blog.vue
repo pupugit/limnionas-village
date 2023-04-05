b06b1b21db6624b7529d5aa0dc20a69353cccdfe
Ferydun Amati <office@amati.solutions>
2023-04-05 11:23:56 +0200

    add blog og info

--- pages/blog.vue
+++ pages/blog.vue
@@ -10,7 +10,44 @@
 <script setup lang="ts">
 import { useI18n } from 'vue-i18n'
 const i18n = useI18n()
-useHead({ title: i18n.t('blog') })
+useHead({
+  title: 'Blog - Limnionas Village',
+  link: [{
+    rel: 'icon',
+    href: 'https://limnionas.netlify.app/favicon.ico',
+    type: 'image/x-icon'
+  }],
+  meta:
+    [{
+      hid: 'description',
+      name: 'description',
+      content: 'Stillen Sie hier Ihre Sehnsucht mit immer wieder neuen Eindrücken aus Limnionas Village.',
+    },
+    {
+      hid: 'og:title',
+      name: 'og:title',
+      property: 'og:title',
+      content: 'Blog - Limnionas Village',
+    },
+    {
+      hid: 'og:site_name',
+      name: 'og:site_name',
+      property: 'og:site_name',
+      content: 'Limnionas Village',
+    },
+    {
+      hid: 'og:image',
+      name: 'og:image',
+      property: 'og:image',
+      content: 'https://limnionas.netlify.app/limvil-picture.png',
+    },
+    {
+      hid: 'og:description',
+      name: 'og:description',
+      property: 'og:description',
+      content: 'Stillen Sie hier Ihre Sehnsucht mit immer wieder neuen Eindrücken aus Limnionas Village.',
+    }],
+})
 const blog = useBlog()
 onMounted(() => {
   initBlog()
