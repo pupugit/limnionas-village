efced465abe32ee6d4d50026ea6b92ee93007f16
Ferydun Amati <office@amati.solutions>
2023-04-16 10:55:28 +0200

    separate blog subpage

--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -1,17 +1,20 @@
 <template>
   <div class="blog-page">
     <div></div>
-    <div class="entries">
-      <BlogEntry v-for="e in blog" :entry="e" :key="e.id" small />
+    <BlogEntry :entry="selectedBlog" v-if="selectedBlog" />
+    <div class="entries" v-else>
+      <BlogEntry v-for="e in blog" :entry="e" :key="e.id" small @click="selectedBlog = e" />
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
 import { useI18n } from 'vue-i18n'
+import type { BlogEntry } from '~/types/blogEntry'
 const i18n = useI18n()
 mergeHead(i18n.locale.value, i18n.t('blog'), 'Satisfy your longing here with new impressions from Limnionas Village.', '')
 const blog = useBlog()
+const selectedBlog = ref<BlogEntry | null>(null)
 onMounted(() => {
   initBlog()
 })
