b0aba9909611106f07bb4d17bb88d825dc779433
Ferydun Amati <office@amati.solutions>
2024-03-28 11:53:44 +0100

    adapt heights

--- components/AboutUsBlock.vue
+++ components/AboutUsBlock.vue
@@ -6,34 +6,38 @@ const props = defineProps<{
   aboutUs: AboutUs,
   showLittleTravelSociety?: boolean
 }>()
-const { width, height } = useWindowSize()
+const width = ref(0)
+const height = ref(0)
 const zenMode = useZenMode()
+
+onMounted(() => {
+  width.value = useWindowSize().width.value
+  height.value = useWindowSize().height.value
+})
 
 </script>
 
 <template>
-  <ClientOnly>
-    <div style="scroll-snap-align: start;">
-      <div v-if="width" :class="`about-us-bg`"
-        :style="`background-image: url(${config.public.directusBase}/assets/${aboutUs.background}?fit=cover&width=${width}&height=${height}&format=${config.public.imageFormat})`">
-        <div class="about-us-content">
-          <div v-html="aboutUs.content"></div>
-          <div v-if="showLittleTravelSociety">
-            <figure role="group" class="little-travel">
-              <a href="https://www.littletravelsociety.de/urlaubsziele/limnionas-village-samos-griechenland/"><img
-                  alt="Logo Little Travel Society" data-entity-type="file"
-                  data-entity-uuid="d623f46d-89f0-418a-b4c7-b1f078153e72" src="/Little_Travel_Society_Link.png"
-                  width="200"></a>
-              <figcaption>Wir freuen uns über die Zusammenarbeit mit Little Travel Society!</figcaption>
-            </figure>
-          </div>
+  <div style="scroll-snap-align: start;">
+    <div v-if="width" :class="`about-us-bg`"
+      :style="`background-image: url(${config.public.directusBase}/assets/${aboutUs.background}?fit=cover&width=${width}&height=${height}&format=${config.public.imageFormat})`">
+      <div class="about-us-content">
+        <div v-html="aboutUs.content"></div>
+        <div v-if="showLittleTravelSociety">
+          <figure role="group" class="little-travel">
+            <a href="https://www.littletravelsociety.de/urlaubsziele/limnionas-village-samos-griechenland/"><img
+                alt="Logo Little Travel Society" data-entity-type="file"
+                data-entity-uuid="d623f46d-89f0-418a-b4c7-b1f078153e72" src="/Little_Travel_Society_Link.png"
+                width="200"></a>
+            <figcaption>Wir freuen uns über die Zusammenarbeit mit Little Travel Society!</figcaption>
+          </figure>
         </div>
       </div>
-      <div v-for="(f, idx) in aboutUs.fotos" v-if="width" :key="idx" class="about-us-foto"
-        :style="`background-image: url(${config.public.directusBase}/assets/${f.directus_files_id}?fit=cover&width=${width}&height=${height}&format=${config.public.imageFormat})`">
-      </div>
     </div>
-  </ClientOnly>
+    <div v-for="(f, idx) in aboutUs.fotos" v-if="width" :key="idx" class="about-us-foto"
+      :style="`background-image: url(${config.public.directusBase}/assets/${f.directus_files_id}?fit=cover&width=${width}&height=${height}&format=${config.public.imageFormat})`">
+    </div>
+  </div>
 </template>
 
 <style>
